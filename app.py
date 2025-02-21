919cff38da42d2a49c9dc52e6a984a02c9b7b6a7
Devansh Khodaskar <devanshkhodaskar870@gmail.com>
2025-02-21 12:29:08 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,9 @@ from flask import Flask, request, render_template, send_from_directory
 import cv2
 import os
 from ultralytics import YOLO
-
+from flask_cors import CORS
 app = Flask(__name__)
-
+CORS(app)
 # Load YOLO segmentation model
 model = YOLO("best.pt")  # Replace with your trained YOLO model
 
